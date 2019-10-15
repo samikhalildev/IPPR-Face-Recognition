805d0da5f017141cfffd982e3d30fe696479aa70
duhhz <42769839+duhhz@users.noreply.github.com>
2019-10-15 22:39:29 +1100

    Update FeatureExtraction.m

--- FeatureExtraction.m
+++ FeatureExtraction.m
@@ -1,14 +1,15 @@
-function [features, labels, personIndex] = FeatureExtraction(data, detector, method)
+function [features, labels, personIndex] = FeatureExtraction(data, method)
 
 counter = 1;
-
 switch method
-    case 'HOG'
+    case 'HOG';
         for i=1:size(data, 2) % 1 to 50
             for j=1:data(i).Count % 1 to 12
-                normalise = rgb2gray(read(data(i),j));
-                histogram = histeq(normalise);
-                %histogram = histeq(read(data(i),j));
+                %if DATABASE / IMAGE IS RGB
+                %normalise = rgb2gray(read(data(i),j));
+                %histogram = histeq(normalise);
+                %else
+                histogram = histeq(read(data(i),j));
                 features(counter,:) = extractHOGFeatures(histogram);
                 labels{counter} = data(i).Description;
                 fprintf('Subject:%i image:%i\n',i, j);
@@ -17,14 +18,18 @@ switch method
             personIndex{i} = data(i).Description;
         end
     
-    case 'LBP'
+    case 'LBP';
         for i=1:size(data, 2) % 1 to 50
             for j=1:data(i).Count % 1 to 12
-                normalise = rgb2gray(read(data(i),j));
-                [a1] = (normalise);
-                meanIntensity = mean(a1(:));
-                a1_binary = a1 > meanIntensity;
-                features(counter,:) = extractLBPFeatures(a1_binary);
+                %if DATABASE / IMAGE IS RGB
+                %normalise = rgb2gray(read(data(i),j));
+                %[a1] = (normalise);
+                %meanIntensity = mean(a1(:));
+                %a1_binary = a1 > meanIntensity;
+                %features(counter,:) = extractLBPFeatures(a1_binary);
+                
+                %else
+                features(counter,:) = extractLBPFeatures(read(data(i),j));
                 labels{counter} = data(i).Description;
                 fprintf('Subject:%i image:%i\n',i, j);
                 counter = counter + 1;
@@ -32,4 +37,4 @@ switch method
             personIndex{i} = data(i).Description;
         end
 end
-end
+
